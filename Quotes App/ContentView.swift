ab4ac1be5618babed047c4bb8fb0a4db588e4e6b
Robert Miller <millerrobert369@aol.com>
2022-04-09 14:18:48 -0400

    Initial Commit

---
+++ Quotes App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quotes App
+//
+//  Created by Robert Miller on 4/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quotes App/Quotes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes_AppApp.swift
+//  Quotes App
+//
+//  Created by Robert Miller on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Quotes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
